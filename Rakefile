49e33f0e3f51f5dc1e383b336c31ed21bca920f4
Louis Machin <ldhmachin@gmail.com>
2022-03-16 10:49:30 +0000

    Added templates

--- Rakefile
+++ Rakefile
@@ -25,8 +25,7 @@ task :heart_grid do
     insert.save_to_file("./example.pdf")
 end
 
-task :good_bad_ideas do
-    # Good Ideas 良いアイデア Bad Ideas 悪いアイデア
+task :good_bad_ideas do # Good Ideas 良いアイデア Bad Ideas 悪いアイデア
     insert = Insert.new(FILOFAX_PERSONAL)
     insert.outlines(FILOFAX_PERSONAL_HOLES)
     width = insert.dimensions.width - 20
@@ -34,5 +33,19 @@ task :good_bad_ideas do
     insert.text("<i>Good Ideas</i>", 4, 6, width, 6)
     insert.box(4, 94, width, 8, "#FFFFD1")
     insert.text("<i>Bad Ideas</i>", 4, 95, width, 6)
+    insert.save_to_file("./example.pdf")
+end
+
+task :project_outline do
+    insert = Insert.new(FILOFAX_PERSONAL)
+    insert.outlines(FILOFAX_PERSONAL_HOLES)
+    ratio = [1, 1, 2, 2]
+    width, height, left = insert.dimensions.width - 18, 6, 2
+    for top in [6, 61, 116] do
+        insert.rows(2, left, top, width, height * 2)
+        insert.ratio_columns(ratio, left, top, width, height)
+        insert.rows(6, left, top + height * 2, width, height * 6)
+        insert.ratio_columns(ratio, left, top + height * 2, width, height * 6)
+    end
     insert.save_to_file("./example.pdf")
 end
--- lib/grid.rb
+++ lib/grid.rb
@@ -1,44 +1,50 @@
 class Insert
-    def ratio_columns(ratios)
-        col_widths = ratios.map { |ratio| ratio * (width / ratios.sum) }
+    def ratio_columns(ratios, l = 0, t = 0, w = 0, h = 0)
+        w = w == 0 ? width : w.mm
+        h = h == 0 ? height : h.mm
+        l, t = l.mm, t.mm
+        col_widths = ratios.map { |ratio| ratio * (w.to_f / ratios.sum) }
         @pdf.stroke do
             @pdf.line_width = (0.1).mm
             col_left = 0
             for col in (0...ratios.length) do
-                x, y = left + col_left, top
-                @pdf.rectangle([x, y], col_widths[col], height)
-                x = double_left + col_left
-                @pdf.rectangle([x, y], col_widths[col], height) if @double
+                x, y = left + l + col_left, top - t
+                @pdf.rectangle([x, y], col_widths[col], h)
+                x = double_left + l + col_left
+                @pdf.rectangle([x, y], col_widths[col], h) if @double
                 col_left += col_widths[col]
             end
         end
     end
 
-    def ratio_rows(ratios)
-        row_heights = ratios.map { |ratio| ratio * (height / ratios.sum) }
+    def ratio_rows(ratios, l = 0, t = 0, w = 0, h = 0)
+        w = w == 0 ? width : w.mm
+        h = h == 0 ? height : h.mm
+        l, t = l.mm, t.mm
+        row_heights = ratios.map { |ratio| ratio * (h.to_f / ratios.sum) }
         @pdf.stroke do
             @pdf.line_width = (0.1).mm
             row_top = 0
             for row in (0...ratios.length) do
-                x, y = left, top - row_top
-                @pdf.rectangle([x, y], width, row_heights[row])
-                x = double_left
-                @pdf.rectangle([x, y], width, row_heights[row]) if @double
+                x, y = left + l, top - t - row_top
+                @pdf.rectangle([x, y], w, row_heights[row])
+                x = double_left + l
+                @pdf.rectangle([x, y], w, row_heights[row]) if @double
                 row_top += row_heights[row]
             end
         end
     end
 
-    def columns(count)
-        ratio_columns([1] * count)
+    def columns(count, l = 0, t = 0, w = 0, h = 0)
+        ratio_columns([1] * count, l, t, w, h)
     end
 
-    def rows(count)
-        ratio_rows([1] * count)
+    def rows(count, l = 0, t = 0, w = 0, h = 0)
+        ratio_rows([1] * count, l, t, w, h)
     end
 
-    def grid(column_count, row_count)
-        columns(column_count)
-        rows(row_count)
+    def grid(column_count, row_count, l = 0, t = 0, w = 0, h = 0)
+        columns(column_count, l, t, w, h)
+        rows(row_count, l, t, w, h)
     end
 end
